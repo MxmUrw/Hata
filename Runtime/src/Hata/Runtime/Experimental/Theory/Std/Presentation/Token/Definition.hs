0bf8900964176ae0653f8d7295bf099cadb4700c
Maxim Urschumzew <u.maxim@live.de>
2021-10-10 17:38:51 +0200

    Switch to megaparsec.

--- Runtime/src/Hata/Runtime/Experimental/Theory/Std/Presentation/Token/Definition.hs
+++ Runtime/src/Hata/Runtime/Experimental/Theory/Std/Presentation/Token/Definition.hs
@@ -5,10 +5,14 @@ module Hata.Runtime.Experimental.Theory.Std.Presentation.Token.Definition where
 
 import Data.Text as T
 import Data.HashMap.Strict as H
+import Data.Void
 
 -- parser
-import Text.Parsec
-import Text.Parsec.Text
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Control.Applicative hiding (many, some)
+
+type Parser = Parsec Void Text
 
 data TokenDefinition a = TokenDefinition
   { constructors :: [a]
@@ -18,15 +22,15 @@ data TokenDefinition a = TokenDefinition
 data Tree a b = Node a [Tree a b] | Var b
 
 doParse :: Parser a -> Text -> Either Text a
-doParse p t = case runParser p () "<source>" (t) of
-                      Left err -> Left $ T.pack $ show err
+doParse p t = case runParser p "<source>" (t) of
+                      Left err -> Left $ T.pack $ errorBundlePretty err
                       Right x -> Right x
 
 parseTokens :: TokenDefinition a -> Text -> Either Text (Tree a Text)
 parseTokens def = doParse (pTokenTree def)
 
 parseTwolines :: TokenDefinition a -> Text -> Either Text (Tree a Text, Tree a Text)
-parseTwolines def t = doParse ((,) <$> (pTokenTree def <* many1 space) <*> (pTokenTree def <* many1 space <* eof)) t
+parseTwolines def t = doParse ((,) <$> (pTokenTree def <* space) <*> (pTokenTree def <* space <* eof)) t
 
 pTokenTree :: forall a. TokenDefinition a -> Parser (Tree a Text)
 pTokenTree def = pTree
@@ -35,7 +39,7 @@ pTokenTree def = pTree
     pTree = try pNode <|> pVar
 
     pParensedTree :: Parser (Tree a Text)
-    pParensedTree = between (char '(') (char ')') pTree
+    pParensedTree = between (char '(') (char ')') pTree <|> pVar
 
     pNode :: Parser (Tree a Text)
     pNode = Node <$> pCon <*> many (pEmpty *> pParensedTree)
@@ -44,16 +48,16 @@ pTokenTree def = pTree
     pCon = choice (p <$> constructors def)
       where
         p :: a -> Parser a
-        p a = try (string (T.unpack $ nameOfCon def a) *> pure a)
+        p a = try (string (nameOfCon def a) *> pure a)
 
     pVar :: Parser (Tree a Text)
-    pVar = Var <$> T.pack <$> many1 pSymbol
+    pVar = Var <$> T.pack <$> some pSymbol
 
     pSymbol :: Parser Char
     pSymbol = noneOf [' ', '\n', '\t', '(' , ')']
 
     pEmpty :: Parser String
-    pEmpty = many1 (char ' ')
+    pEmpty = some (char ' ')
 
 
 -------
